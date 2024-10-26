34a244b8876ba273d1722a9e0aa35ec42fcd1027
OtsukaSeri <kd1326982@st.kobedenshi.ac.jp>
2024-10-26 20:36:58 +0900

    データベース1

--- database/seeders/DetailTableSeeder.php
+++ database/seeders/DetailTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use DateTime;
 
 class DetailTableSeeder extends Seeder
 {
@@ -14,7 +15,12 @@ class DetailTableSeeder extends Seeder
     public function run(): void
     {
         DB::table('details')->insert([
-            
+            ['user_id' => '1',
+             'content' => '食費',
+             'amount' => 2900,
+             'created_at' => new DateTime(),
+             'updated_at' => new DateTime(),
+             'deleted_at' => null,],
         ]);
     }
 }
