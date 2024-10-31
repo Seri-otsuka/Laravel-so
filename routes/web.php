76f12b913bf84f355dd321bee7fa3e28795e4506
OtsukaSeri <kd1326982@st.kobedenshi.ac.jp>
2024-10-31 12:02:11 +0900

    テスト

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DetailController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/index', function () {
-    return view('index');
-});
+Route::get('/index',[DetailController::class,'detail'])->name('detail');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
